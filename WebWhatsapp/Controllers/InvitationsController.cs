fa1fdd06f16ff00e44f607e5fd68f534e80a62c0
SivanJhirad <sivanush550@gamil.com>
2022-08-15 12:31:51 +0300

    Add project files.

---
+++ WebWhatsapp/Controllers/InvitationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebWhatsappApi.Service;
+using WebWhatsappApi.Models;
+
+namespace WebWhatsappApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InvitationsController : Controller
+    {
+        ContactService contactsService = new ContactService();
+        
+        [HttpPost]
+        public IActionResult Invitations(Invitation invitation)
+        {
+            ContactToAdd contactToAdd = new ContactToAdd();
+            contactToAdd.Id = invitation.from;
+            contactToAdd.Server = invitation.server;
+
+            contactToAdd.Name = invitation.from;
+
+            string userId = invitation.to;
+
+            Boolean added = contactsService.AddToDB(userId, contactToAdd);
+
+            if (added)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ WhatsappProject/WhatsappProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WhatsappProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
